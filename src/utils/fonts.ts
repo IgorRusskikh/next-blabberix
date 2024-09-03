d52c99238452d45adbf668f235d8f8f9732fdad0
IgorRusskikh <russkikh200@mail.ru>
2024-09-03 23:28:53 +0700

    feat: added utils for fonts

---
+++ src/utils/fonts.ts
@@ -0,0 +1,11 @@
+import localFont from "next/font/local";
+
+export const openSans600 = localFont({
+  src: "../../public/fonts/OpenSans-VariableFont_wdth,wght.ttf",
+  weight: "600",
+});
+
+export const obrazec800 = localFont({
+  src: "../../public/fonts/Obrazec 2.woff2",
+  weight: "800",
+});

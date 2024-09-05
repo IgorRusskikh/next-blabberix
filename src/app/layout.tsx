d2c106192c1868920f4ba8268fe196458166eb3d
IgorRusskikh <russkikh200@mail.ru>
2024-09-05 21:32:32 +0700

    feat: added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,13 @@ import "./globals.css";
 
 import type { Metadata, Viewport } from "next";
 
-export const metadata: Metadata = {};
+export const metadata: Metadata = {
+  title: "BLABBERIX",
+  description:
+    "Discover BLABBERIX, the groundbreaking social network in Web3 that seamlessly integrates cutting-edge artificial intelligence and tokenomics technologies. With no current equivalent in the market, BLABBERIX is not just a product—it's a revolutionary idea in social networking.",
+  keywords:
+    "BLABBERIX, Web3 social network, AI integration, tokenomics, blockchain social media, innovative social networking, artificial intelligence, decentralized social platforms, Web3 technologies, future of social media",
+};
 
 export const dynamic = "force-static";
 

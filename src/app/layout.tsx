11b4681e11fed0d520600e0d37d8add5addcef59
IgorRusskikh <russkikh200@mail.ru>
2024-09-05 20:18:28 +0700

    feat: added viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import "./globals.css";
 
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 
 export const metadata: Metadata = {};
 
 export const dynamic = "force-static";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
 
 export default function RootLayout({
   children,
